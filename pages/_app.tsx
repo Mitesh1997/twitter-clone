f9da8f5bf7499d03612fff477d0147682f81f800
Your Name <you@example.com>
2024-05-26 18:41:18 +0530

    one

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Inter } from "next/font/google";
+import {GoogleOAuthProvider} from "@react-oauth/google"
 const inter = Inter({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <div className={inter.className}> <Component {...pageProps} /></div>;
+  return <div className={inter.className}> 
+  <GoogleOAuthProvider clientId="223717560456-fplcnthsu1id1ac26laab5p0ir4q4369.apps.googleusercontent.com">
+  <Component {...pageProps} />
+  </GoogleOAuthProvider>
+  </div>;
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useCallback } from "react";
 import Image from "next/image";
 import { BsBell, BsBookmark, BsEnvelope, BsTwitter } from "react-icons/bs";
 import { BiHash, BiHomeCircle, BiMoney, BiUser } from "react-icons/bi";
 import FeedCard from "@/components/FeedCard";
 import { SlOptions } from "react-icons/sl";
+import {CredentialResponse, GoogleLogin} from "@react-oauth/google"
 
 interface TwitterSidebarButton {
   title: string;
@@ -45,6 +46,9 @@ const sidebarMenuItems: TwitterSidebarButton[] = [
   },
 ];
 export default function Home() {
+
+  const handleLoginWithGoogle=useCallback((cred: CredentialResponse)=>{},[])
+
   return (
     <div>
       <div className="grid grid-cols-12 h-screen w-screen px-56">
@@ -80,7 +84,12 @@ export default function Home() {
           <FeedCard />
           <FeedCard />
         </div>
-        <div className="col-span-3 "></div>
+        <div className="col-span-3 p-5 ">
+          <div className="p-5 bg-slate-700 rounded-lg ">
+            <h1 className="">New to Twitter</h1>
+          <GoogleLogin onSuccess={(cred)=>console.log(cred)}/>
+        </div>
+        </div>
       </div>
     </div>
   );
